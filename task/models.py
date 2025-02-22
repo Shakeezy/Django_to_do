f9880e76fdd6b704c86ab7c0dab3dbca2ee7d7fe
Shakeezy <163127790+Shakeezy@users.noreply.github.com>
2025-02-22 17:36:15 +0100

    Delete task directory

--- task/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from autentification.models import CustomUser
-
-class Task(models.Model):
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    title = models.CharField(max_length=100, blank=False, null=False)
-    done = models.BooleanField(default=False)
-    def __str__(self):
-        return self.title
--- task/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('tasks', views.tasks_template, name='tasks_template'),
-    path('delete/<int:id>', views.delete, name='delete'),
-    path('state/<int:id>', views.state, name='state'),
-]
